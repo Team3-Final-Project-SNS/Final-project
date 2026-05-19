f8e1c51802428da00cd3e04c99972c7d67c71bfa
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-19 17:35:46 +0900

    feat: 로그인 기능에 필요한 UserService 및 UserServiceImpl파일 작성

--- src/main/java/com/example/team3final/domain/user/service/UserService.java
+++ src/main/java/com/example/team3final/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.team3final.domain.user.service;
 
+import com.example.team3final.domain.user.entity.User;
+
 public interface UserService {
 
     // 이메일로 사용자 ID를 조회합니다.
@@ -8,4 +10,6 @@ public interface UserService {
 
     // 회원가입 시 가입되어있는 이메일인지 검증
     boolean isEmailAlreadyRegistered(String email);
+
+    User findByEmail(String email);
 }
