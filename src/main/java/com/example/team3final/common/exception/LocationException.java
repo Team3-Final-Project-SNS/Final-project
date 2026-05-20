1e1ebe2d49072a1d798ad125fdfe3b7c14f022d2
Ho-jin98 <hohoj98@naver.com>
2026-05-20 15:11:19 +0900

    feat: LocationException 생성

---
+++ src/main/java/com/example/team3final/common/exception/LocationException.java
@@ -0,0 +1,8 @@
+package com.example.team3final.common.exception;
+
+public class LocationException extends ServiceException {
+
+    public LocationException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
