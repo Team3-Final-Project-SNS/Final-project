94429527843a0c2606925c9f51a57948e3012859
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-21 10:30:02 +0900

    feat: 내정보 수정 controller 코드 작성

--- src/main/java/com/example/team3final/domain/user/controller/UserController.java
+++ src/main/java/com/example/team3final/domain/user/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.example.team3final.domain.user.controller;
 
 import com.example.team3final.common.dto.response.ApiResponseDto;
+import com.example.team3final.domain.user.dto.request.UpdateUserRequestDto;
 import com.example.team3final.domain.user.dto.response.GetUserResponseDto;
+import com.example.team3final.domain.user.dto.response.UpdateUserResponseDto;
 import com.example.team3final.domain.user.service.UserDetailsImpl;
 import com.example.team3final.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +25,19 @@ public class UserController {
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long userId = userDetails.getUserId();
         GetUserResponseDto response = userService.getUser(userId);
+
+        return ResponseEntity.ok(ApiResponseDto.success(response));
+    }
+
+    // 내 정보 수정
+    @PatchMapping("/me")
+    public ResponseEntity<ApiResponseDto<UpdateUserResponseDto>> updateUser(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @Valid @RequestBody UpdateUserRequestDto request
+    ) {
+        // JWT에서 검증된 userId 추출
+        Long userId = userDetails.getUserId();
+        UpdateUserResponseDto response = userService.updateUser(userId, request);
 
         return ResponseEntity.ok(ApiResponseDto.success(response));
     }
