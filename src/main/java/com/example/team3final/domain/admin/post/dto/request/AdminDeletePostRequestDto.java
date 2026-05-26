41a394255e1f9a14cbaa7087fc6f22574dc31a8c
Ho-jin98 <hohoj98@naver.com>
2026-05-26 19:13:53 +0900

    fix: dto 필드값 수정

--- src/main/java/com/example/team3final/domain/admin/post/dto/request/AdminDeletePostRequestDto.java
+++ src/main/java/com/example/team3final/domain/admin/post/dto/request/AdminDeletePostRequestDto.java
@@ -1,10 +1,14 @@
 package com.example.team3final.domain.admin.post.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class AdminDeletePostRequestDto {
+
+    @NotNull(message = "신고 ID는 필수입니다.")
+    private Long reportId;
 
     @NotBlank(message = "삭제 사유는 필수입니다.")
     private String reason;
