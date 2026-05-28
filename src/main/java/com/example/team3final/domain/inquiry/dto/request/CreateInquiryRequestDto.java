5d33f61be46e411aa70d732900edf1240586f1bc
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-28 11:50:36 +0900

    feat: 고객문의 Dto 생성

---
+++ src/main/java/com/example/team3final/domain/inquiry/dto/request/CreateInquiryRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.team3final.domain.inquiry.dto.request;
+
+public class CreateInquiryReqestDto {
+}
---
+++ src/main/java/com/example/team3final/domain/inquiry/dto/response/CreateInquiryResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.team3final.domain.inquiry.dto.response;
+
+public class CreateInquiryResponseDto {
+}
