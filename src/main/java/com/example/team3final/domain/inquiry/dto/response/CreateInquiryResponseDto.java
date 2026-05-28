6a091d66341a3abf1a0d3095fffc3eec854bb648
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-28 11:50:47 +0900

    feat: 고객문의 Dto 생성

--- src/main/java/com/example/team3final/domain/inquiry/dto/response/CreateInquiryResponseDto.java
+++ src/main/java/com/example/team3final/domain/inquiry/dto/response/CreateInquiryResponseDto.java
@@ -1,4 +1,20 @@
 package com.example.team3final.domain.inquiry.dto.response;
 
-public class CreateInquiryResponseDto {
+import com.example.team3final.domain.inquiry.entity.Inquiry;
+import com.example.team3final.domain.inquiry.enums.InquiryAnswerStatus;
+
+import java.time.LocalDateTime;
+
+public record CreateInquiryResponseDto (
+        Long inquiryId,
+        InquiryAnswerStatus status,
+        LocalDateTime createdAt
+) {
+    public static CreateInquiryResponseDto from(Inquiry inquiry) {
+        return new CreateInquiryResponseDto(
+                inquiry.getId(),
+                inquiry.getAnswerStatus(),
+                inquiry.getCreatedAt()
+        );
+    }
 }
