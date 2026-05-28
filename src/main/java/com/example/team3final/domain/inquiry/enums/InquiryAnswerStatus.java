0f17ea5880b4fb07f3722972b4a5964bec9942d1
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-28 19:05:23 +0900

    fix: Status 수정

--- src/main/java/com/example/team3final/domain/inquiry/enums/InquiryAnswerStatus.java
+++ src/main/java/com/example/team3final/domain/inquiry/enums/InquiryAnswerStatus.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum InquiryAnswerStatus {
 
     PENDING("접수완료"),
-    IN_PROGRESS("확인함"),
+    READ("열람"),
     ANSWERED("답변완료"),
     CLOSED("종결"),
     WITHDRAWN("취소");
