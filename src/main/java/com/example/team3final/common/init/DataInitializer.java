2e0932f5cbafb9442cd8b61ae0efb5243e61512b
godchm <choehyeongmin7@gmail.com>
2026-05-28 19:01:42 +0900

    주석 추가

--- src/main/java/com/example/team3final/common/init/DataInitializer.java
+++ src/main/java/com/example/team3final/common/init/DataInitializer.java
@@ -63,6 +63,7 @@ public class DataInitializer implements ApplicationRunner {
     @Transactional
     public void run(ApplicationArguments args) {
 
+        // 데이터 중복을 피하기 위해서. 설정.
         if (universityRepository.existsByeDomainAndIsActiveTrue("korea.ac.kr")) {
             return;
         }
