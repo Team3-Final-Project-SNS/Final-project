1eb2779874aaec67c089a4db09258caf2f5971a2
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-06-01 21:49:20 +0900

    fix: 한국시간으로 설정 변경

--- src/main/java/com/example/team3final/Team3FinalApplication.java
+++ src/main/java/com/example/team3final/Team3FinalApplication.java
@@ -1,5 +1,6 @@
 package com.example.team3final;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,12 +8,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties
 @EnableScheduling      // 스케줄러 활성화
 @EnableAsync           // 알림 발송 (NotificationPublisher) 비동기 처리 활성화
 public class Team3FinalApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Team3FinalApplication.class, args);
