f3ed74ff07985f3c7173e1138a2f027eff471928
Ho-jin98 <hohoj98@naver.com>
2026-06-05 12:39:08 +0900

    feat: 수정 사항 반영

--- src/main/java/com/example/team3final/common/init/DataInitializer.java
+++ src/main/java/com/example/team3final/common/init/DataInitializer.java
@@ -39,7 +39,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Profile("local")
+@Profile({"prod","local"})
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationRunner {
