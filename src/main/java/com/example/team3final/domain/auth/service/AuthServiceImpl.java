f916a3f5ca4fae8b49df4daa54bf25c2db0c2433
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-06-12 21:32:18 +0900

    fix: 하드코딩 된 TTL 수정

--- src/main/java/com/example/team3final/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/example/team3final/domain/auth/service/AuthServiceImpl.java
@@ -285,7 +285,7 @@ public class AuthServiceImpl implements AuthService{
         redisTemplate.opsForValue().set(
                 REFRESH_TOKEN_KEY_PREFIX + savedUser.getEmail(),
                 refreshToken,
-                Duration.ofMillis(14L * 24 * 60 * 60 * 1000) // 14일 (ms 단위)
+                Duration.ofMillis(jwtProvider.getRefreshTokenValidityTime())
         );
 
         // refresh_token HttpOnly 쿠키 발급
@@ -333,7 +333,7 @@ public class AuthServiceImpl implements AuthService{
         redisTemplate.opsForValue().set(
                 REFRESH_TOKEN_KEY_PREFIX + user.getEmail(),
                 refreshToken,
-                Duration.ofMillis(14L * 24 * 60 * 60 * 1000) // 14일
+                Duration.ofMillis(jwtProvider.getRefreshTokenValidityTime())
         );
 
         // Refresh Token을 HttpOnly 쿠키로 응답에 추가
@@ -379,7 +379,7 @@ public class AuthServiceImpl implements AuthService{
         redisTemplate.opsForValue().set(
                 REFRESH_TOKEN_KEY_PREFIX + email,
                 newRefreshToken,
-                Duration.ofMillis(14L * 24 * 60 * 60 * 1000)
+                Duration.ofMillis(jwtProvider.getRefreshTokenValidityTime())
         );
 
         // 7. 새 Refresh Token 쿠키로 재전송
@@ -441,7 +441,7 @@ public class AuthServiceImpl implements AuthService{
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
-        cookie.setMaxAge(14 * 24 * 60 * 60); // 1209600초 (14일) 고정
+        cookie.setMaxAge((int) (jwtProvider.getRefreshTokenValidityTime() / 1000));
         response.addCookie(cookie);
     }
 
