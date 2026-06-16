a5c14261e0cebc03a33efdd0fce6ba8b36030ff0
Ho-jin98 <hohoj98@naver.com>
2026-06-16 16:53:14 +0900

    fix: 게시글 삭제/복구에 따라 포인트 변동 수정

--- src/main/java/com/example/team3final/domain/post/service/PostModerationServiceImpl.java
+++ src/main/java/com/example/team3final/domain/post/service/PostModerationServiceImpl.java
@@ -63,7 +63,7 @@ public class PostModerationServiceImpl implements PostModerationService {
 
         // 삭제 때 작성자에게 환불했으므로, 복구 시 다시 차감
         // 잔액 부족 시 user.deduct() 내부에서 예외 발생
-        userPointService.deductPoint(post.getAuthorId(), redepositPoint, null);
+        userPointService.redepositAuthorDeposit(post.getAuthorId(), redepositPoint, post.getId());
 
         // 게시글 복구
         post.restore();
